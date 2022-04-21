7743e35bccbd9ac5c1721a31c013c34a54156d65
kcharles2013 <kcharles2013@hotmail.com>
2022-04-20 18:24:59 -0700

    logic to detect user's browser

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,28 @@ import Button from "./button";
 // let recursion = 0;
 
 function Footer(){
+    let possibleBrowser = ['Chrome', 'Firefox', 'Safari'];
+    // const ua = navigator.userAgent;
+    const ba = navigator.userAgent;
 
-    if (navigator.userAgent.indexOf("Safari") !== -1){
+    // alert(ba);
+
+    if (ba.indexOf(possibleBrowser[2]) > -1){
         document.getElementById('footer-container').classList.add('safari-mobile-browser');
+        alert('You are using a safari-browser');
     }
+
+    // for (let i = 0; i < possibleBrowser.length; i++){
+    //     if (ba.indexOf(ba[i]) > -1){
+    //
+    //     }
+    // }
+    alert(`userAgent: ${ba}`);
+    // alert(`browser.family: ${ua.browser.family.toString()}`);
+    // alert(`os.family: ${ua.os.family.toString()}`);
+    // if (navigator.userAgent.indexOf("Safari") !== -1){
+    //     document.getElementById('footer-container').classList.add('safari-mobile-browser');
+    // }
     return(
         <div className="footer-container" id='footer-container'>
             <div className='button-divider' id='button-divider'>
